b08124fe4d3acb4dd97fffab9c2799d1f01879eb
Trevor Burbidge <Trevor.Burbidge@myresman.onmicrosoft.com>
2023-08-23 12:38:43 -0600

    Add LanguageService

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorDemo.Client;
+using BlazorDemo.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ILanguageService, LanguageService>();
 
 await builder.Build().RunAsync();
