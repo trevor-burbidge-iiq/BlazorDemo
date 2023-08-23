12f436be02cbf1cf2625b07ea7d942845acf63f6
Trevor Burbidge <Trevor.Burbidge@myresman.onmicrosoft.com>
2023-08-23 11:40:21 -0600

    Add project files.

---
+++ Server/Controllers/LanguagesController.cs
@@ -0,0 +1,30 @@
+﻿using BlazorDemo.Shared.ApiModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDemo.Server.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class LanguagesController : ControllerBase
+    {
+        private static List<LanguageApiModel> languages = new List<LanguageApiModel>()
+        {
+            new LanguageApiModel() { Name = "C#", HasWasmCompiler = true },
+            new LanguageApiModel() { Name = "C", HasWasmCompiler = true },
+            new LanguageApiModel() { Name = "Java", HasWasmCompiler = false },
+        };
+
+        [HttpGet]
+        public List<LanguageApiModel> Get()
+        {
+            return languages;
+        }
+
+        [HttpPost]
+        public IActionResult Post(LanguageApiModel model)
+        {
+            languages.Add(model);
+            return Ok();
+        }
+    }
+}
---
+++ Shared/ApiModels/LanguageApiModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorDemo.Shared.ApiModels
+{
+    public class LanguageApiModel
+    {
+        public string Name { get; set; }
+        public bool HasWasmCompiler { get; set; }
+    }
+}
